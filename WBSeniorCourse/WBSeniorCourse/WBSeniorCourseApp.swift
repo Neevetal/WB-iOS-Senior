46dacf96d5aacf80b720bbe0461520687666b50a
Vitaly Malkov <prugyn@gmail.com>
2024-06-29 21:17:03 +0300

    main #comment create project

---
+++ WBSeniorCourse/WBSeniorCourse/WBSeniorCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSeniorCourseApp.swift
+//  WBSeniorCourse
+//
+//  Created by Vitaly Malkov on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSeniorCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
