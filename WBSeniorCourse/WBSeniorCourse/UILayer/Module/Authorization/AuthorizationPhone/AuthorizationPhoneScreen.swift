6a5404c0564b857939e1a0bfe85b10b739ca37d1
Vitaly Malkov <prugyn@gmail.com>
2024-06-29 22:33:32 +0300

    WBS-1 #comment create architecture

---
+++ WBSeniorCourse/WBSeniorCourse/UILayer/Module/Authorization/AuthorizationPhone/AuthorizationPhoneScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorizationPhoneScreen.swift
+//  WBSeniorCourse
+//
+//  Created by Vitaly Malkov on 29.06.2024.
+//
+
+import SwiftUI
+
+struct AuthorizationPhoneScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthorizationPhoneScreen()
+}
---
+++ WBSeniorCourse/WBSeniorCourse/UILayer/View/Image/RoundImage.swift
@@ -0,0 +1,18 @@
+//
+//  RoundImage.swift
+//  WBSeniorCourse
+//
+//  Created by Vitaly Malkov on 29.06.2024.
+//
+
+import SwiftUI
+
+struct RoundImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundImage()
+}
