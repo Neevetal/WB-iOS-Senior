f215ea04999f82ccc94f180f6d9e7839ee061534
Vitaly Malkov <prugyn@gmail.com>
2024-08-22 23:27:29 +0300

    WBS-15 #comment create watchOS target

---
+++ WBSeniorCourse/WatchWBSeniorCourse Watch App/WatchWBSeniorCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWBSeniorCourseApp.swift
+//  WatchWBSeniorCourse Watch App
+//
+//  Created by Vitaly Malkov on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchWBSeniorCourse_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
