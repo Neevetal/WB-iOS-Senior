8b10f14de1319e74de740901231d4dabf9d7e1ba
Vitaly Malkov <prugyn@gmail.com>
2024-09-03 12:11:58 +0300

    WBS-16 #comment add tvOS

---
+++ WBSeniorCourse/WatchWBSeniorCourse/WatchWBSeniorCourseApp.swift
@@ -0,0 +1,21 @@
+//
+//  WatchWBSeniorCourseApp.swift
+//  WatchWBSeniorCourse
+//
+//  Created by Vitaly Malkov on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchWBSeniorCourseApp: App {
+    
+    // MARK: - Body
+    
+    var body: some Scene {
+        WindowGroup {
+            ToDoListView()
+                .modelContainer(for: ToDoItem.self)
+        }
+    }
+}
